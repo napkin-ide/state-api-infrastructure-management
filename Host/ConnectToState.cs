9609e69351c087ddea654660753137fec7e002ba
mcgear <michael.gearhardt@gmail.com>
2020-04-24 09:34:42 -0600

    Upgrades

--- Host/ConnectToState.cs
+++ Host/ConnectToState.cs
@@ -12,17 +12,20 @@ using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using System.Security.Claims;
 using LCU.StateAPI;
 using LCU.State.API.NapkinIDE.InfrastructureManagement.State;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace LCU.State.API.NapkinIDE.InfrastructureManagement.Host
 {
     public static class ConnectToState
     {
         [FunctionName("ConnectToState")]
-        public static async Task<ConnectToStateResponse> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req, ILogger logger, 
-            ClaimsPrincipal claimsPrincipal, [DurableClient] IDurableEntityClient entity,
-            [SignalR(HubName = InfrastructureManagementState.HUB_NAME)]IAsyncCollector<SignalRGroupAction> signalRGroupActions)
+        public static async Task<ConnectToStateResponse> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req, ILogger log,
+            ClaimsPrincipal claimsPrincipal, //[LCUStateDetails]StateDetails stateDetails,
+            [SignalR(HubName = InfrastructureManagementState.HUB_NAME)]IAsyncCollector<SignalRMessage> signalRMessages,
+            [SignalR(HubName = InfrastructureManagementState.HUB_NAME)]IAsyncCollector<SignalRGroupAction> signalRGroupActions,
+            [Blob("state-api/{headers.lcu-ent-api-key}/{headers.lcu-hub-name}/{headers.x-ms-client-principal-id}/{headers.lcu-state-key}", FileAccess.ReadWrite)] CloudBlockBlob stateBlob)
         {
-            return await entity.ConnectToState<InfrastructureManagementState>(req, logger, claimsPrincipal, signalRGroupActions);
+            return await signalRMessages.ConnectToState<InfrastructureManagementState>(req, log, claimsPrincipal, stateBlob, signalRGroupActions);
         }
     }
 }
